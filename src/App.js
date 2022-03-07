1e73435c54afc9d2938ac96502bafac650e48cc3
Khanz1998 <zahrakhan282@gmail.com>
2022-03-07 15:51:13 +0000

    Added Github repository + LinkedIn links

--- src/App.js
+++ src/App.js
@@ -5,11 +5,31 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="App Weather">
-      <header className="App-header">
-        <h1>Weather Wise</h1>
-        <Weather />
-      </header>
+    <div>
+      <div className="App Weather">
+        <header className="App-header">
+          <h1>Weather Wise</h1>
+          <Weather />{" "}
+        </header>
+      </div>
+      <small className="social-links">
+        This project is{" "}
+        <a
+          href="https://github.com/Khanz1998/week4-react-project"
+          target="_blank"
+          rel="norefermer"
+        >
+          open sourced
+        </a>
+        , by{" "}
+        <a
+          href="https://www.linkedin.com/in/zahrakhan1998/"
+          target="_blank"
+          rel="norefermer"
+        >
+          Zahra Khan
+        </a>
+      </small>
     </div>
   );
 }
